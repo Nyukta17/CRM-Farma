7f0e27703a35ddeee115cbc2c50c8d0604bb33c0
maksim_ivanov <max.ivan.97@mail.ru>
2026-04-10 22:10:34 +0300

    12

--- frontend/src/components/Pages/Orders.tsx
+++ frontend/src/components/Pages/Orders.tsx
@@ -60,25 +60,45 @@ const testDate: IOrder[] = [
 ]
 
 const statusStyles = {
-  'Новый': 'bg-blue-100 text-blue-800 border-blue-200',
-  'Оплачен': 'bg-green-100 text-green-800 border-green-200',
-  'Собран': 'bg-yellow-100 text-yellow-800 border-yellow-200',
-  'Отправлен': 'bg-purple-100 text-purple-800 border-purple-200',
+    'Новый': 'bg-blue-100 text-blue-800 border-blue-200',
+    'Оплачен': 'bg-green-100 text-green-800 border-green-200',
+    'Собран': 'bg-yellow-100 text-yellow-800 border-yellow-200',
+    'Отправлен': 'bg-purple-100 text-purple-800 border-purple-200',
 };
-const columns:{key: keyof IOrder;lable:string}[]=[
-    {key:'id',lable:''},
-    {key:'date',lable:''},
-    {key:'client',lable:''},
-    {key:'order',lable:''},
-    {key:'payment',lable:''},
-    {key:'status',lable:''}
+const columns: { key: keyof IOrder; lable: string }[] = [
+    { key: 'id', lable: '№' },
+    { key: 'date', lable: 'Дата' },
+    { key: 'client', lable: 'Клиент' },
+    { key: 'order', lable: 'заказ' },
+    { key: 'payment', lable: 'Оплата' },
+    { key: 'status', lable: 'Статус' }
 ]
 
 const Orders = () => {
     return (<>
         <table>
-            <thead></thead>
-            <tbody></tbody>
+            <thead>
+                <tr>
+                    { columns.map((col)=>
+                        <th>
+                            {col.lable}
+                        </th>
+                    )}
+                </tr>
+            </thead>
+            <tbody>
+                 {testDate.map((row)=>
+                <tr key={row.id}>
+                   
+                       {columns.map((col)=>
+                    <td key={col.key}>
+                    </td>
+                    )}
+                    
+    
+                </tr>
+                 )}
+            </tbody>
         </table>
     </>)
 }
